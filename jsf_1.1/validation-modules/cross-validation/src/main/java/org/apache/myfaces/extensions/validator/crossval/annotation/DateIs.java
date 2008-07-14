6fad3e908d16b61c74770198509e201bb56775bd
Gerhard Petracek <gpetracek@apache.org>
2008-07-14 17:58:09 +0000

    initial import of MyFaces-Extensions-Validator
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@676664 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jsf_1.1/validation-modules/cross-validation/src/main/java/org/apache/myfaces/extensions/validator/crossval/annotation/DateIs.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.crossval.annotation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+import java.text.DateFormat;
+
+/**
+ * @author Gerhard Petracek
+ */
+@Target({METHOD, FIELD})
+@Retention(RUNTIME)
+//TODO DateIsEntry (value, type)
+public @interface DateIs {
+    String[] valueOf();
+
+    /*
+     * optional section
+     */
+
+    DateIsType type() default DateIsType.same;
+
+    String validationErrorMsgKey() default "";
+
+    String notBeforeErrorMsgKey() default "wrong_date_not_before";
+
+    String notAfterErrorMsgKey() default "wrong_date_not_after";
+
+    String notEqualErrorMsgKey() default "wrong_date_not_equal";
+
+    int errorMessageDateStyle() default DateFormat.MEDIUM;
+}
