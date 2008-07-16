0fff11a59d24b940aa4089d540543a0a082cd9bb
Gerhard Petracek <gpetracek@apache.org>
2008-07-16 20:54:51 +0000

    merged validation modules (see dev-list discussion)
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@677425 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jsf_1.1/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/crossval/strategy/EqualsStrategy.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.crossval.strategy;
+
+import org.apache.myfaces.extensions.validator.crossval.CrossValidationStorageEntry;
+import org.apache.myfaces.extensions.validator.crossval.annotation.Equals;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * @author Gerhard Petracek
+ */
+//TODO test custom message bundle
+public class EqualsStrategy extends AbstractCompareStrategy {
+
+    public boolean useTargetComponentToDisplayErrorMsg(CrossValidationStorageEntry crossValidationStorageEntry) {
+        return true;
+    }
+
+    protected String getValidationErrorMsgKey(Annotation annotation, boolean isTargetComponent) {
+        return ((Equals) annotation).validationErrorMsgKey();
+    }
+
+    public boolean isViolation(Object object1, Object object2, Annotation annotation) {
+        return object1 != null && !object1.equals(object2);
+    }
+
+    public String[] getValidationTargets(Annotation annotation) {
+        return ((Equals) annotation).value();
+    }
+}
