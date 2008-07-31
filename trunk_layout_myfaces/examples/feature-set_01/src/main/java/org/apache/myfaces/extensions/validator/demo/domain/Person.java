2128cce8b07935f115397f89e88f236c44e490aa
Leonardo Uribe <lu4242@apache.org>
2008-07-31 18:33:27 +0000

    apply checkstyle rules
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@681433 13f79535-47bb-0310-9956-ffa450edef68

--- trunk_layout_myfaces/examples/feature-set_01/src/main/java/org/apache/myfaces/extensions/validator/demo/domain/Person.java
+++ trunk_layout_myfaces/examples/feature-set_01/src/main/java/org/apache/myfaces/extensions/validator/demo/domain/Person.java
@@ -1,20 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
  */
 package org.apache.myfaces.extensions.validator.demo.domain;
 
@@ -29,7 +29,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
 
-public class Person {
+public class Person
+{
     @Length(minimum = 2)
     @Column(nullable = false, length = 20)
     @NotEquals("lastName")
@@ -63,67 +64,83 @@ public class Person {
     @Column(nullable = false)
     private int numberOfSiblings;
 
-    public String getFirstName() {
+    public String getFirstName()
+    {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName)
+    {
         this.firstName = firstName;
     }
 
-    public String getLastName() {
+    public String getLastName()
+    {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName)
+    {
         this.lastName = lastName;
     }
 
-    public String getPassword() {
+    public String getPassword()
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password)
+    {
         this.password = password;
     }
 
-    public Date getBirthday() {
+    public Date getBirthday()
+    {
         return birthday;
     }
 
-    public void setBirthday(Date birthday) {
+    public void setBirthday(Date birthday)
+    {
         this.birthday = birthday;
     }
 
-    public Date getFinalExam() {
+    public Date getFinalExam()
+    {
         return finalExam;
     }
 
-    public void setFinalExam(Date finalExam) {
+    public void setFinalExam(Date finalExam)
+    {
         this.finalExam = finalExam;
     }
 
-    public String getNickName() {
+    public String getNickName()
+    {
         return nickName;
     }
 
-    public void setNickName(String nickName) {
+    public void setNickName(String nickName)
+    {
         this.nickName = nickName;
     }
 
-    public String getEmail() {
+    public String getEmail()
+    {
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String email)
+    {
         this.email = email;
     }
 
-    public int getNumberOfSiblings() {
+    public int getNumberOfSiblings()
+    {
         return numberOfSiblings;
     }
 
-    public void setNumberOfSiblings(int numberOfSiblings) {
+    public void setNumberOfSiblings(int numberOfSiblings)
+    {
         this.numberOfSiblings = numberOfSiblings;
     }
 }
