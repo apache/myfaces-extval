d8928e54b034b9a7805fe38335420f43e433bb70
Gerhard Petracek <gpetracek@apache.org>
2008-09-10 20:46:03 +0000

    branch to prototype client side validation
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@693974 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/trunk_client_side_validation/core/src/main/java/org/apache/myfaces/extensions/validator/core/adapter/DefaultConverterAdapterFactory.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.core.adapter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.myfaces.extensions.validator.core.ClassMappingFactory;
+import org.apache.myfaces.extensions.validator.core.adapter.mapper.CustomConfiguredConverterToAdapterNameMapper;
+import org.apache.myfaces.extensions.validator.core.adapter.mapper.CustomConventionConverterToAdapterNameMapper;
+import org.apache.myfaces.extensions.validator.core.adapter.mapper.DefaultConverterToAdapterNameMapper;
+import org.apache.myfaces.extensions.validator.core.adapter.mapper.SimpleConverterToAdapterNameMapper;
+import org.apache.myfaces.extensions.validator.core.mapper.NameMapper;
+import org.apache.myfaces.extensions.validator.internal.UsageEnum;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.util.ClassUtils;
+
+import javax.faces.convert.Converter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * just for the fallback solution
+ *
+ * @author Gerhard Petracek
+ */
+@UsageInformation(UsageEnum.FALLBACK)
+public class DefaultConverterAdapterFactory implements
+    ClassMappingFactory<Converter, Converter>
+{
+    private static Map<String, String> converterAdapterMapping = new HashMap<String, String>();
+    private static List<NameMapper<Converter>> nameMapperList = new ArrayList<NameMapper<Converter>>();
+
+    static
+    {
+        nameMapperList.add(new CustomConfiguredConverterToAdapterNameMapper());
+        nameMapperList.add(new CustomConventionConverterToAdapterNameMapper());
+        nameMapperList.add(new DefaultConverterToAdapterNameMapper());
+        nameMapperList.add(new SimpleConverterToAdapterNameMapper());
+    }
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public Converter create(Converter converter)
+    {
+        String converterName = converter.getClass().getName();
+
+        if (converterAdapterMapping.containsKey(converterName))
+        {
+            return (Converter) ClassUtils
+                .tryToInstantiateClassForName(converterAdapterMapping
+                    .get(converterName));
+        }
+
+        Converter adapter;
+        String adapterName;
+
+        for (NameMapper<Converter> nameMapper : nameMapperList)
+        {
+            adapterName = nameMapper.createName(converter);
+            adapter = (Converter) ClassUtils
+                .tryToInstantiateClassForName(adapterName);
+
+            if (adapter != null)
+            {
+                addMapping(converterName, adapter.getClass().getName());
+
+                if (this.logger.isTraceEnabled())
+                {
+                    this.logger.trace("used adapter: " + adapter.getClass().getName());
+                }
+
+                return adapter;
+            }
+        }
+
+        if (this.logger.isDebugEnabled())
+        {
+            this.logger.debug("no adapter found for " + converterName
+                + " -> converter itself is used -> no sev-en support");
+        }
+        return converter;
+    }
+
+    private void addMapping(String sourceConverter, String adapter)
+    {
+        synchronized (DefaultConverterAdapterFactory.class)
+        {
+            converterAdapterMapping.put(sourceConverter, adapter);
+        }
+        //TODO logging
+    }
+}
---
+++ branches/trunk_client_side_validation/core/src/main/java/org/apache/myfaces/extensions/validator/util/FactoryUtils.java
@@ -0,0 +1,169 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.util;
+
+import org.apache.myfaces.extensions.validator.core.ClassMappingFactory;
+import org.apache.myfaces.extensions.validator.core.WebXmlParameter;
+import org.apache.myfaces.extensions.validator.core.adapter.DefaultConverterAdapterFactory;
+import org.apache.myfaces.extensions.validator.core.annotation.extractor.AnnotationExtractorFactory;
+import org.apache.myfaces.extensions.validator.core.annotation.extractor.DefaultAnnotationExtractorFactory;
+import org.apache.myfaces.extensions.validator.core.validation.message.resolver.DefaultMessageResolverFactory;
+import org.apache.myfaces.extensions.validator.core.validation.message.resolver.MessageResolver;
+import org.apache.myfaces.extensions.validator.core.validation.strategy.DefaultValidationStrategyFactory;
+import org.apache.myfaces.extensions.validator.core.validation.strategy.ValidationStrategy;
+
+import javax.faces.convert.Converter;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Gerhard Petracek
+ */
+//TODO
+public class FactoryUtils
+{
+    private static AnnotationExtractorFactory annotationExtractorFactory;
+
+    public static AnnotationExtractorFactory getAnnotationExtractorFactory()
+    {
+        if (annotationExtractorFactory == null)
+        {
+            List<String> annotationExtractorFactoryClassNames = new ArrayList<String>();
+
+            annotationExtractorFactoryClassNames
+                .add(WebXmlParameter.CUSTOM_ANNOTATION_EXTRACTOR_FACTORY);
+            annotationExtractorFactoryClassNames.add(ExtValUtils
+                .getInformationProviderBean()
+                .getCustomAnnotationExtractorFactory());
+            annotationExtractorFactoryClassNames
+                .add(DefaultAnnotationExtractorFactory.class.getName());
+
+            for (String className : annotationExtractorFactoryClassNames)
+            {
+                annotationExtractorFactory = (AnnotationExtractorFactory) ClassUtils
+                    .tryToInstantiateClassForName(className);
+
+                if (annotationExtractorFactory != null)
+                {
+                    //TODO logging
+                    break;
+                }
+            }
+        }
+
+        return annotationExtractorFactory;
+    }
+
+    private static ClassMappingFactory<Annotation, ValidationStrategy> validationStrategyFactory;
+
+    public static ClassMappingFactory<Annotation, ValidationStrategy> getValidationStrategyFactory()
+    {
+        if (validationStrategyFactory == null)
+        {
+            List<String> validationStrategyFactoryClassNames = new ArrayList<String>();
+
+            validationStrategyFactoryClassNames
+                .add(WebXmlParameter.CUSTOM_VALIDATION_STRATEGY_FACTORY);
+            validationStrategyFactoryClassNames.add(ExtValUtils
+                .getInformationProviderBean()
+                .getCustomValidationStrategyFactory());
+            validationStrategyFactoryClassNames
+                .add(DefaultValidationStrategyFactory.class.getName());
+
+            for (String className : validationStrategyFactoryClassNames)
+            {
+                validationStrategyFactory = (ClassMappingFactory<Annotation, ValidationStrategy>) ClassUtils
+                    .tryToInstantiateClassForName(className);
+
+                if (validationStrategyFactory != null)
+                {
+                    //TODO logging
+                    break;
+                }
+            }
+        }
+
+        return validationStrategyFactory;
+    }
+
+    private static ClassMappingFactory<ValidationStrategy, MessageResolver> messageResolverFactory;
+
+    public static ClassMappingFactory<ValidationStrategy, MessageResolver> getMessageResolverFactory()
+    {
+        if (messageResolverFactory == null)
+        {
+            List<String> messageResolverFactoryClassNames = new ArrayList<String>();
+
+            messageResolverFactoryClassNames
+                .add(WebXmlParameter.CUSTOM_MESSAGE_RESOLVER_FACTORY);
+            messageResolverFactoryClassNames.add(ExtValUtils
+                .getInformationProviderBean()
+                .getCustomMessageResolverFactory());
+            messageResolverFactoryClassNames
+                .add(DefaultMessageResolverFactory.class.getName());
+
+            for (String className : messageResolverFactoryClassNames)
+            {
+                messageResolverFactory = (ClassMappingFactory<ValidationStrategy, MessageResolver>) ClassUtils
+                    .tryToInstantiateClassForName(className);
+
+                if (messageResolverFactory != null)
+                {
+                    //TODO logging
+                    break;
+                }
+            }
+        }
+
+        return messageResolverFactory;
+    }
+
+    private static ClassMappingFactory<Converter, Converter> converterAdapterFactory;
+
+    @Deprecated
+    public static ClassMappingFactory<Converter, Converter> getConverterAdapterFactory()
+    {
+        if (converterAdapterFactory == null)
+        {
+            List<String> converterAdapterFactoryClassNames = new ArrayList<String>();
+
+            converterAdapterFactoryClassNames
+                .add(WebXmlParameter.CUSTOM_CONVERTER_ADAPTER_FACTORY);
+            converterAdapterFactoryClassNames.add(ExtValUtils
+                .getInformationProviderBean()
+                .getCustomConverterAdapterFactory());
+            converterAdapterFactoryClassNames
+                .add(DefaultConverterAdapterFactory.class.getName());
+
+            for (String className : converterAdapterFactoryClassNames)
+            {
+                converterAdapterFactory = (ClassMappingFactory<Converter, Converter>) ClassUtils
+                    .tryToInstantiateClassForName(className);
+
+                if (converterAdapterFactory != null)
+                {
+                    //TODO logging
+                    break;
+                }
+            }
+        }
+        return converterAdapterFactory;
+    }
+}
