b03ba3d30d4db53298da511c3f864cebb019e6ee
Gerhard Petracek <gpetracek@apache.org>
2008-09-23 10:36:39 +0000

    support for multiple DefaultComponentInitializer implementations
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@698126 13f79535-47bb-0310-9956-ffa450edef68

--- jsf_1.1/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/HtmlCoreComponentsComponentInitializer.java
+++ jsf_1.1/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/HtmlCoreComponentsComponentInitializer.java
@@ -18,7 +18,6 @@
  */
 package org.apache.myfaces.extensions.validator;
 
-import org.apache.myfaces.extensions.validator.util.ReflectionUtils;
 import org.apache.myfaces.extensions.validator.core.initializer.component.ComponentInitializer;
 
 import javax.faces.component.EditableValueHolder;
@@ -31,11 +30,8 @@ import java.util.Map;
  * @author Gerhard Petracek
  * @since 1.x.1
  */
-public class DefaultMyFacesComponentInitializer implements ComponentInitializer
+public class HtmlCoreComponentsComponentInitializer implements ComponentInitializer
 {
-    private static final String TRINIDAD_CORE_INPUT_TEXT
-                                    = "org.apache.myfaces.trinidad.component.core.input.CoreInputText";
-
     public void configureComponent(FacesContext facesContext, UIComponent uiComponent, Map<String, Object> metaData)
     {
         configureRequiredAttribute(facesContext, uiComponent, metaData);
@@ -48,7 +44,7 @@ public class DefaultMyFacesComponentInitializer implements ComponentInitializer
     {
         if(metaData.containsKey(MetaDataKeys.REQUIRED))
         {
-            if((Boolean)metaData.get(MetaDataKeys.REQUIRED) && isComponentRequired( uiComponent ))
+            if((Boolean)metaData.get(MetaDataKeys.REQUIRED) && Boolean.TRUE.equals(isComponentRequired(uiComponent)))
             {
                 ((EditableValueHolder)uiComponent).setRequired(true);
             }
@@ -62,23 +58,18 @@ public class DefaultMyFacesComponentInitializer implements ComponentInitializer
      * @param uiComponent component which implements the EditableValueHolder interface
      * @return false to overrule the annotated property e.g. if component is readonly
      */
-    protected boolean isComponentRequired(UIComponent uiComponent)
+    protected Boolean isComponentRequired(UIComponent uiComponent)
     {
         if(uiComponent instanceof HtmlInputText)
         {
             HtmlInputText htmlInputText = (HtmlInputText)uiComponent;
-            return !(htmlInputText.isDisabled() || htmlInputText.isReadonly());
-        }
-        else if(TRINIDAD_CORE_INPUT_TEXT.equals(uiComponent.getClass().getName()))
-        {
-            return !(Boolean) ReflectionUtils
-                .tryToInvokeMethodOfClassAndMethodName(TRINIDAD_CORE_INPUT_TEXT, "isReadOnly");
+            return !(htmlInputText.isReadonly() || htmlInputText.isDisabled());
         }
 
-        return true;
+        return null;
     }
 
-    private void configureMaxLengthAttribute(FacesContext facesContext,
+    protected void configureMaxLengthAttribute(FacesContext facesContext,
                                              UIComponent uiComponent,
                                              Map<String, Object> metaData)
     {
@@ -91,13 +82,6 @@ public class DefaultMyFacesComponentInitializer implements ComponentInitializer
                 HtmlInputText htmlInputText = (HtmlInputText)uiComponent;
                 htmlInputText.setMaxlength(maxLength);
             }
-            else if(TRINIDAD_CORE_INPUT_TEXT.equals(uiComponent.getClass().getName()))
-            {
-                ReflectionUtils
-                    .tryToInvokeMethodOfClassAndMethodName(TRINIDAD_CORE_INPUT_TEXT, "setMaximumLength",
-                        new Object[] {maxLength}, new Class[] {int.class});
-            }
-
         }
     }
 }
