899a8abf113589d3ab74a34fbfad90cc75f90188
Gerhard Petracek <gpetracek@apache.org>
2008-09-24 20:49:07 +0000

    restructured trinidad support
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@698726 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/jsf_1.1/component-support/trinidad-support/src/main/java/org/apache/myfaces/extensions/validator/initializer/trinidad/InitTrinidadModulePhaseListener.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.initializer.trinidad;
+
+import org.apache.myfaces.extensions.validator.core.AbstractStartupConfigListener;
+import org.apache.myfaces.extensions.validator.initializer.trinidad.component.TrinidadComponentInitializer;
+import org.apache.myfaces.extensions.validator.initializer.trinidad.rendering.TrinidadRenderingContextInitializer;
+import org.apache.myfaces.extensions.validator.internal.Priority;
+import org.apache.myfaces.extensions.validator.internal.ToDo;
+import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+
+/**
+ * @author Gerhard Petracek
+ */
+public class InitTrinidadModulePhaseListener extends AbstractStartupConfigListener
+{
+    @ToDo(value = Priority.MEDIUM, description = "web.xml parameter to deactivate it")
+    protected void init()
+    {
+        ExtValUtils.addRenderingContextInitializer(new TrinidadRenderingContextInitializer());
+
+        String deactivateClientSideValidation = WebXmlParameter.DEACTIVATE_CLIENT_SIDE_TRINIDAD_VALIDATION;
+
+        if(deactivateClientSideValidation == null || !deactivateClientSideValidation.equalsIgnoreCase("true"))
+        {
+            ExtValUtils.addComponentInitializer(new TrinidadComponentInitializer());
+        }
+    }
+}
