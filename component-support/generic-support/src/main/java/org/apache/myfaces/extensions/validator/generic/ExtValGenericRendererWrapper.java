3e0c5ed06d2ffda7c952858d518e43454acd6a4a
Gerhard Petracek <gpetracek@apache.org>
2008-09-28 14:27:25 +0000

    minor refactoring
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/trunk@699821 13f79535-47bb-0310-9956-ffa450edef68

--- component-support/generic-support/src/main/java/org/apache/myfaces/extensions/validator/generic/ExtValGenericRendererWrapper.java
+++ component-support/generic-support/src/main/java/org/apache/myfaces/extensions/validator/generic/ExtValGenericRendererWrapper.java
@@ -20,15 +20,13 @@ package org.apache.myfaces.extensions.validator.generic;
 
 import org.apache.myfaces.extensions.validator.internal.UsageCategory;
 import org.apache.myfaces.extensions.validator.internal.UsageInformation;
-import org.apache.myfaces.extensions.validator.core.ExtValContext;
-import org.apache.myfaces.extensions.validator.core.RendererInterceptor;
-import org.apache.myfaces.extensions.validator.core.RendererProxy;
+import org.apache.myfaces.extensions.validator.core.ExtValRendererWrapper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
-import javax.faces.convert.ConverterException;
 import javax.faces.render.Renderer;
-import java.io.IOException;
 import java.lang.reflect.Method;
 
 import net.sf.cglib.proxy.MethodInterceptor;
@@ -40,10 +38,9 @@ import net.sf.cglib.proxy.MethodProxy;
  * @since 1.x.1
  */
 @UsageInformation(UsageCategory.INTERNAL)
-public class ExtValGenericRendererWrapper extends Renderer implements MethodInterceptor
+public final class ExtValGenericRendererWrapper extends ExtValRendererWrapper implements MethodInterceptor
 {
-    protected Renderer wrapped;
-    protected ExtValContext extValContext = ExtValContext.getContext();
+    private static final Log log = LogFactory.getLog(ExtValGenericRendererWrapper.class);
 
     public static Renderer newInstance(Renderer renderer)
     {
@@ -65,27 +62,27 @@ public class ExtValGenericRendererWrapper extends Renderer implements MethodInte
 
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable
     {
-        if (method.getName().equals("getConvertedValue"))
+        if (method.getName().equals("getConvertedValue") && args[1] instanceof UIComponent)
         {
             return getConvertedValue((FacesContext)args[0], (UIComponent)args[1], args[2]);
         }
-        else if (method.getName().equals("decode"))
+        else if (method.getName().equals("decode") && args[1] instanceof UIComponent)
         {
             decode((FacesContext)args[0], (UIComponent)args[1]);
         }
-        else if (method.getName().equals("encodeBegin"))
+        else if (method.getName().equals("encodeBegin") && args[1] instanceof UIComponent)
         {
             encodeBegin((FacesContext)args[0], (UIComponent)args[1]);
         }
-        else if (method.getName().equals("encodeChildren"))
+        else if (method.getName().equals("encodeChildren") && args[1] instanceof UIComponent)
         {
             encodeChildren((FacesContext)args[0], (UIComponent)args[1]);
         }
-        else if (method.getName().equals("encodeEnd"))
+        else if (method.getName().equals("encodeEnd") && args[1] instanceof UIComponent)
         {
             encodeEnd((FacesContext)args[0], (UIComponent)args[1]);
         }
-        else if (method.getName().equals("convertClientId"))
+        else if (method.getName().equals("convertClientId") && args[1] instanceof String)
         {
             return convertClientId((FacesContext)args[0], (String)args[1]);
         }
@@ -95,6 +92,10 @@ public class ExtValGenericRendererWrapper extends Renderer implements MethodInte
         }
         else
         {
+            if(log.isTraceEnabled())
+            {
+                log.trace("method " + method.getName() + " called without rendering-interceptors");
+            }
             return proxy.invokeSuper(obj, args);
         }
         return null;
@@ -102,127 +103,6 @@ public class ExtValGenericRendererWrapper extends Renderer implements MethodInte
 
     public ExtValGenericRendererWrapper(Renderer wrapped)
     {
-        this.wrapped = new RendererProxy(wrapped);
-    }
-
-    @Override
-    public final void decode(FacesContext facesContext, UIComponent uiComponent)
-    {
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.beforeDecode(facesContext, uiComponent, this.wrapped);
-        }
-
-        wrapped.decode(facesContext, uiComponent);
-
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.afterDecode(facesContext, uiComponent, this.wrapped);
-        }
-    }
-
-    @Override
-    public final void encodeBegin(FacesContext facesContext, UIComponent uiComponent)
-        throws IOException
-    {
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.beforeEncodeBegin(facesContext, uiComponent, this.wrapped);
-        }
-
-        wrapped.encodeBegin(facesContext, uiComponent);
-
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.afterEncodeBegin(facesContext, uiComponent, this.wrapped);
-        }
-    }
-
-    @Override
-    public final void encodeChildren(FacesContext facesContext, UIComponent uiComponent)
-        throws IOException
-    {
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.beforeEncodeChildren(facesContext, uiComponent, this.wrapped);
-        }
-
-        wrapped.encodeChildren(facesContext, uiComponent);
-
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.afterEncodeChildren(facesContext, uiComponent, this.wrapped);
-        }
-    }
-
-    @Override
-    public final void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
-        throws IOException
-    {
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.beforeEncodeEnd(facesContext, uiComponent, this.wrapped);
-        }
-
-        wrapped.encodeEnd(facesContext, uiComponent);
-
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.afterEncodeEnd(facesContext, uiComponent, this.wrapped);
-        }
-    }
-
-    @Override
-    public final String convertClientId(FacesContext facesContext, String s)
-    {
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.beforeConvertClientId(facesContext, s, this.wrapped);
-        }
-
-        String clientId = wrapped.convertClientId(facesContext, s);
-
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.afterConvertClientId(facesContext, s, this.wrapped);
-        }
-
-        return clientId;
-    }
-
-    @Override
-    public final boolean getRendersChildren()
-    {
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.beforeGetRendersChildren(this.wrapped);
-        }
-
-        boolean rendersChildren = wrapped.getRendersChildren();
-
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.afterGetRendersChildren(this.wrapped);
-        }
-
-        return rendersChildren;
-    }
-
-    public Object getConvertedValue(FacesContext facesContext, UIComponent uiComponent, Object o)
-        throws ConverterException
-    {
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.beforeGetConvertedValue(facesContext, uiComponent, o, this.wrapped);
-        }
-
-        Object convertedObject = wrapped.getConvertedValue(facesContext, uiComponent, o);
-
-        for(RendererInterceptor rendererInterceptor : extValContext.getRendererInterceptors())
-        {
-            rendererInterceptor.afterGetConvertedValue(facesContext, uiComponent, o, this.wrapped);
-        }
-
-        return convertedObject;
+        super(wrapped);
     }
 }
