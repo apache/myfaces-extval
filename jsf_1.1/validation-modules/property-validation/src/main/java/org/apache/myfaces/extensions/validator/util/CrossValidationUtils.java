ed67c0bccf3ea1458191c839628c714b5cb25e00
Gerhard Petracek <gpetracek@apache.org>
2008-09-29 00:13:09 +0000

    decoupled cross-validation infrastructure
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@699926 13f79535-47bb-0310-9956-ffa450edef68

--- jsf_1.1/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/util/CrossValidationUtils.java
+++ jsf_1.1/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/util/CrossValidationUtils.java
@@ -19,9 +19,11 @@
 package org.apache.myfaces.extensions.validator.util;
 
 import org.apache.myfaces.extensions.validator.crossval.CrossValidationStorage;
+import org.apache.myfaces.extensions.validator.crossval.ProcessedInformationEntry;
 
 import javax.faces.context.FacesContext;
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * @author Gerhard Petracek
@@ -52,5 +54,25 @@ public class CrossValidationUtils
                 .getExternalContext()
                 .getRequestMap()
                 .put(CROSS_VALIDATION_STORAGE_KEY, new CrossValidationStorage());
+    }
+
+    public static final String VALUE_BINDING_CONVERTED_VALUE_MAPPING_KEY = ExtValUtils.class.getName();
+
+    public static Map<String, ProcessedInformationEntry> getOrInitValueBindingConvertedValueMapping()
+    {
+        Map requestMap = FacesContext.getCurrentInstance().getExternalContext().getRequestMap();
+
+        if (!requestMap.containsKey(VALUE_BINDING_CONVERTED_VALUE_MAPPING_KEY))
+        {
+            resetValueBindingConvertedValueMapping();
+        }
+
+        return (Map<String, ProcessedInformationEntry>) requestMap.get(VALUE_BINDING_CONVERTED_VALUE_MAPPING_KEY);
+    }
+
+    public static void resetValueBindingConvertedValueMapping()
+    {
+        FacesContext.getCurrentInstance().getExternalContext().getRequestMap()
+            .put(VALUE_BINDING_CONVERTED_VALUE_MAPPING_KEY, new HashMap<String, ProcessedInformationEntry>());
     }
 }
