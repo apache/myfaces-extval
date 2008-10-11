409279d50ed8686e2e280b6a36dee35f209bdd94
Gerhard Petracek <gpetracek@apache.org>
2008-10-11 19:54:36 +0000

    state before generic meta-data validation
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches/annotation_only_based_validation@703719 13f79535-47bb-0310-9956-ffa450edef68

---
+++ 1.0.1-SNAPSHOT/core/src/main/java/org/apache/myfaces/extensions/validator/core/renderkit/ExtValRenderKit.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.core.renderkit;
+
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.faces.context.ResponseStream;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.RenderKit;
+import javax.faces.render.Renderer;
+import javax.faces.render.ResponseStateManager;
+import java.io.OutputStream;
+import java.io.Writer;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@UsageInformation(UsageCategory.INTERNAL)
+public class ExtValRenderKit extends RenderKit
+{
+    protected final Log logger = LogFactory.getLog(getClass());
+    protected RenderKit wrapped;
+
+    public ExtValRenderKit(RenderKit wrapped)
+    {
+        this.wrapped = wrapped;
+
+        if(logger.isDebugEnabled())
+        {
+            logger.debug(getClass().getName() + " instantiated");
+        }
+    }
+
+    public void addRenderer(String family, String rendererType, Renderer renderer)
+    {
+        if (renderer instanceof ExtValRendererWrapper)
+        {
+            wrapped.addRenderer(family, rendererType, renderer);
+        }
+        else
+        {
+            wrapped.addRenderer(family, rendererType, createWrapper(renderer));
+        }
+    }
+
+    public Renderer getRenderer(String family, String rendererType)
+    {
+        Renderer renderer = wrapped.getRenderer(family, rendererType);
+        return renderer instanceof ExtValRendererWrapper ? renderer : createWrapper(renderer);
+    }
+
+    public ResponseStateManager getResponseStateManager()
+    {
+        return wrapped.getResponseStateManager();
+    }
+
+    public ResponseWriter createResponseWriter(Writer writer, String s, String s1)
+    {
+        return wrapped.createResponseWriter(writer, s, s1);
+    }
+
+    public ResponseStream createResponseStream(OutputStream outputStream)
+    {
+        return wrapped.createResponseStream(outputStream);
+    }
+
+    @UsageInformation({UsageCategory.REUSE, UsageCategory.CUSTOMIZABLE})
+    protected Renderer createWrapper(Renderer renderer)
+    {
+        return new ExtValRendererWrapper(renderer);
+    }
+}
---
+++ 1.0.1-SNAPSHOT/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/baseval/metadata/transformer/JoinMetaDataTransformer.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.baseval.metadata.transformer;
+
+import org.apache.myfaces.extensions.validator.baseval.annotation.JoinValidation;
+import org.apache.myfaces.extensions.validator.baseval.annotation.extractor.DefaultPropertyScanningAnnotationExtractor;
+import org.apache.myfaces.extensions.validator.core.annotation.AnnotationEntry;
+import org.apache.myfaces.extensions.validator.core.annotation.extractor.AnnotationExtractor;
+import org.apache.myfaces.extensions.validator.core.metadata.transformer.MetaDataTransformer;
+import org.apache.myfaces.extensions.validator.core.metadata.transformer.AbstractMetaDataTransformer;
+import org.apache.myfaces.extensions.validator.core.validation.strategy.ValidationStrategy;
+import org.apache.myfaces.extensions.validator.core.el.ValueBindingExpression;
+import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+
+import javax.faces.context.FacesContext;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+public class JoinMetaDataTransformer  extends AbstractMetaDataTransformer
+{
+    protected Map<String, Object> convert(AnnotationEntry annotationEntry)
+    {
+        AnnotationExtractor extractor = new DefaultPropertyScanningAnnotationExtractor();
+
+        String[] targetExpressions = ((JoinValidation)(annotationEntry).getAnnotation()).value();
+
+        ValidationStrategy validationStrategy;
+        MetaDataTransformer metaDataTransformer;
+
+        Map<String, Object> results = new HashMap<String, Object>();
+
+        for (String targetExpression : targetExpressions)
+        {
+            targetExpression = createValidBinding(annotationEntry, targetExpression);
+
+            for (AnnotationEntry entry : extractor
+                                            .extractAnnotations(FacesContext.getCurrentInstance(), targetExpression))
+            {
+                validationStrategy = ExtValUtils.getValidationStrategyForAnnotation(entry.getAnnotation());
+
+                metaDataTransformer = ExtValUtils.getMetaDataTransformerForValidationStrategy(validationStrategy);
+
+                if (metaDataTransformer != null)
+                {
+                    results.putAll(metaDataTransformer.convertMetaData(entry));
+                }
+            }
+        }
+        return results;
+    }
+
+    private String createValidBinding(AnnotationEntry annotationEntry, String targetExpression)
+    {
+        if(ExtValUtils.getELHelper().isELTerm(targetExpression))
+        {
+            return targetExpression;
+        }
+        
+        ValueBindingExpression baseExpression = new ValueBindingExpression(annotationEntry.getValueBindingExpression());
+        return ValueBindingExpression.replaceOrAddProperty(baseExpression, targetExpression).getExpressionString();
+    }
+}
