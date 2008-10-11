a41d7dcef74911633229bffc7811aeb9e19bf072
Gerhard Petracek <gpetracek@apache.org>
2008-10-11 19:54:36 +0000

    state before generic meta-data validation
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@703719 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/annotation_only_based_validation/1.0.1-SNAPSHOT/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/crossval/annotation/extractor/DefaultValueBindingScanningAnnotationExtractor.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.crossval.annotation.extractor;
+
+import org.apache.myfaces.extensions.validator.core.annotation.AnnotationEntry;
+import org.apache.myfaces.extensions.validator.core.annotation.extractor.DefaultComponentAnnotationExtractor;
+import org.apache.myfaces.extensions.validator.core.el.ValueBindingExpression;
+import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+
+import javax.faces.context.FacesContext;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * to support the usage of vb-xpressions (to reference the target bean)
+ *
+ * @author Gerhard Petracek
+ */
+public class DefaultValueBindingScanningAnnotationExtractor extends
+        DefaultComponentAnnotationExtractor
+{
+
+    @Override
+    public List<AnnotationEntry> extractAnnotations(FacesContext facesContext,
+            Object object)
+    {
+        //should never occur
+        if (!(object instanceof String))
+        {
+            return new ArrayList<AnnotationEntry>();
+        }
+
+        ValueBindingExpression valueBindingExpression = new ValueBindingExpression(((String) object).trim());
+
+        List<AnnotationEntry> annotationEntries = new ArrayList<AnnotationEntry>();
+
+        Class entity = ExtValUtils.getELHelper().getTypeOfValueBindingForExpression(facesContext,
+                valueBindingExpression);
+
+        if (entity != null)
+        {
+            //find and add annotations
+            addPropertyAccessAnnotations(entity, annotationEntries, valueBindingExpression.getExpressionString());
+            addFieldAccessAnnotations(entity, annotationEntries, valueBindingExpression.getExpressionString());
+        }
+
+        return annotationEntries;
+    }
+
+    protected void addPropertyAccessAnnotations(Class entity,
+            List<AnnotationEntry> annotationEntries,
+            String valueBindingExpression)
+    {
+        AnnotationEntry templateEntry;
+
+        for (Method method : entity.getDeclaredMethods())
+        {
+            templateEntry = new AnnotationEntry();
+            templateEntry.setEntityClass(entity.getClass());
+            templateEntry.setValueBindingExpression(valueBindingExpression);
+            templateEntry.setBoundTo("[method]:" + method.getName());
+
+            addAnnotationToAnnotationEntries(annotationEntries, Arrays
+                    .asList(method.getAnnotations()), templateEntry);
+        }
+    }
+
+    protected void addFieldAccessAnnotations(Class entity,
+            List<AnnotationEntry> annotationEntries,
+            String valueBindingExpression)
+    {
+        AnnotationEntry templateEntry;
+
+        for (Field field : entity.getDeclaredFields())
+        {
+            templateEntry = new AnnotationEntry();
+            templateEntry.setEntityClass(entity.getClass());
+            templateEntry.setValueBindingExpression(valueBindingExpression);
+            templateEntry.setBoundTo("[field]:" + field.getName());
+
+            addAnnotationToAnnotationEntries(annotationEntries, Arrays
+                    .asList(field.getAnnotations()), templateEntry);
+        }
+    }
+}
---
+++ branches/annotation_only_based_validation/1.0.1-SNAPSHOT/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/crossval/recorder/CrossValidationUserInputRecorder.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.crossval.recorder;
+
+import org.apache.myfaces.extensions.validator.core.recorder.ProcessedInformationRecorder;
+import org.apache.myfaces.extensions.validator.core.el.ValueBindingExpression;
+import org.apache.myfaces.extensions.validator.crossval.ProcessedInformationEntry;
+import org.apache.myfaces.extensions.validator.util.CrossValidationUtils;
+import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.EditableValueHolder;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @author Gerhard Petracek
+ */
+public class CrossValidationUserInputRecorder implements ProcessedInformationRecorder
+{
+    public void recordUserInput(UIComponent uiComponent, Object value)
+    {
+        if (!(uiComponent instanceof EditableValueHolder))
+        {
+            return;
+        }
+
+        //to support local cross-validation (within the same entity)
+        Map<String, ProcessedInformationEntry> valueBindingConvertedValueMapping = CrossValidationUtils
+            .getOrInitValueBindingConvertedValueMapping();
+
+        ProcessedInformationEntry entry;
+
+        ValueBindingExpression vbe =
+            ExtValUtils.getELHelper().getValueBindingExpression(uiComponent);
+
+        if (vbe == null)
+        {
+            return;
+        }
+
+        entry = new ProcessedInformationEntry();
+        entry.setBean(ExtValUtils.getELHelper().getBaseObject(vbe, uiComponent));
+        entry.setConvertedValue(value);
+        entry.setComponent(uiComponent);
+
+        String key = vbe.getExpressionString();
+        //for local cross-validation
+        if (valueBindingConvertedValueMapping.containsKey(key) &&
+            !valueBindingConvertedValueMapping.get(key).getBean().equals(entry.getBean()))
+        {
+            //for the validation within a complex component e.g. a table
+            //don't override existing expression (style: #{entry.property}) - make a special mapping
+
+            List<ProcessedInformationEntry> furtherEntries =
+                valueBindingConvertedValueMapping.get(key).getFurtherEntries();
+
+            if (furtherEntries == null)
+            {
+                furtherEntries = new ArrayList<ProcessedInformationEntry>();
+
+                valueBindingConvertedValueMapping.get(key).setFurtherEntries(furtherEntries);
+            }
+
+            furtherEntries.add(entry);
+        }
+        else
+        {
+            //for normal validation
+            valueBindingConvertedValueMapping.put(key, entry);
+        }
+    }
+}
