469173980dbc0f096c959c0fab8287ca4ce5c25c
Gerhard Petracek <gpetracek@apache.org>
2008-11-30 20:29:00 +0000

    branch for extval 1.2.1 release preparation
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@721890 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/1_2_1prepare/examples/hello_world/src/main/webapp/helloWorld.jsp
@@ -0,0 +1,26 @@
+<%@ taglib uri="http://java.sun.com/jsf/html" prefix="h" %>
+<%@ taglib uri="http://java.sun.com/jsf/core" prefix="f"%>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <f:view>
+            <h:form id="mainForm">
+              <h:panelGrid columns="3">
+                <h:outputLabel for="first_name" value="First name:"/>
+                <h:inputText id="first_name" label="First name" value="#{helloWorld.person.firstName}"/>
+                <h:message for="first_name" showSummary="true" showDetail="false"/>
+
+                <h:outputLabel for="last_name" value="Last name:"/>
+                <h:inputText id="last_name" label="Last name" value="#{helloWorld.person.lastName}"/>
+                <h:message for="last_name" showSummary="true" showDetail="false"/>
+
+                <h:commandButton value="Press me" action="#{helloWorld.send}"/>
+                <h:panelGroup/>
+                <h:panelGroup/>
+              </h:panelGrid>
+            </h:form>
+        </f:view>
+    </body>
+</html>
---
+++ branches/1_2_1prepare/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/crossval/strategy/NotEqualsStrategy.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.crossval.strategy;
+
+import org.apache.myfaces.extensions.validator.crossval.annotation.NotEquals;
+import org.apache.myfaces.extensions.validator.baseval.annotation.SkipValidationSupport;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@SkipValidationSupport
+@UsageInformation(UsageCategory.INTERNAL)
+public class NotEqualsStrategy extends EqualsStrategy
+{
+    @Override
+    protected String getValidationErrorMsgKey(Annotation annotation, boolean isTargetComponent)
+    {
+        return ((NotEquals) annotation).validationErrorMsgKey();
+    }
+
+    @Override
+    public boolean isViolation(Object object1, Object object2,
+            Annotation annotation)
+    {
+        return !super.isViolation(object1, object2, annotation);
+    }
+
+    @Override
+    public String[] getValidationTargets(Annotation annotation)
+    {
+        return ((NotEquals) annotation).value();
+    }
+}
