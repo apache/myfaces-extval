e790e56aa8c8031b82062d08374410b9d6521a9c
Gerhard Petracek <gpetracek@apache.org>
2008-12-03 21:05:43 +0000

    [maven-release-plugin]  copy for tag 1_0_1_rc
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@723073 13f79535-47bb-0310-9956-ffa450edef68

---
+++ 1_0_1_rc/component-support/trinidad-support/src/main/java/org/apache/myfaces/extensions/validator/trinidad/initializer/component/LongRangeInitializer.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.trinidad.initializer.component;
+
+import org.apache.myfaces.extensions.validator.core.metadata.CommonMetaDataKeys;
+import org.apache.myfaces.extensions.validator.internal.ToDo;
+import org.apache.myfaces.extensions.validator.internal.Priority;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.trinidad.validator.LongRangeValidator;
+
+import javax.faces.component.EditableValueHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import java.util.Map;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@ToDo(value = Priority.MEDIUM, description = "skipValidationSupport for client-side validation")
+@UsageInformation(UsageCategory.INTERNAL)
+public class LongRangeInitializer extends TrinidadComponentInitializer
+{
+    @Override
+    public boolean configureTrinidadComponent(FacesContext facesContext, UIComponent uiComponent,
+                                              Map<String, Object> metaData)
+    {
+        boolean informationAdded = false;
+        LongRangeValidator lengthValidator = (LongRangeValidator)facesContext.getApplication()
+                                            .createValidator("org.apache.myfaces.trinidad.LongRange");
+
+        if(metaData.containsKey(CommonMetaDataKeys.RANGE_MIN))
+        {
+            Object min = metaData.get(CommonMetaDataKeys.RANGE_MIN);
+
+            if(min instanceof Long)
+            {
+                lengthValidator.setMinimum((Long)min);
+                informationAdded = true;
+            }
+        }
+
+        if(metaData.containsKey(CommonMetaDataKeys.RANGE_MAX))
+        {
+            Object maxLength = metaData.get(CommonMetaDataKeys.RANGE_MAX);
+
+            if(maxLength instanceof Long)
+            {
+                lengthValidator.setMaximum((Long)maxLength);
+                informationAdded = true;
+            }
+        }
+        if(informationAdded)
+        {
+            ((EditableValueHolder)uiComponent).addValidator(lengthValidator);
+            return true;
+        }
+        return false;
+    }
+}
