5b611e5285baf6be3b2775c56a7eb70a85f6cb8e
Gerhard Petracek <gpetracek@apache.org>
2008-12-03 02:39:39 +0000

    branch to clean up build issues caused by the maven build plug-in
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@722722 13f79535-47bb-0310-9956-ffa450edef68

---
+++ prepare1_0_1rc_test/core/src/main/java/org/apache/myfaces/extensions/validator/core/ExtValContext.java
@@ -0,0 +1,336 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.core;
+
+import org.apache.myfaces.extensions.validator.core.initializer.component.ComponentInitializer;
+import org.apache.myfaces.extensions.validator.core.initializer.configuration.StaticConfiguration;
+import org.apache.myfaces.extensions.validator.core.interceptor.RendererInterceptor;
+import org.apache.myfaces.extensions.validator.core.interceptor.ValidationExceptionInterceptor;
+import org.apache.myfaces.extensions.validator.core.recorder.ProcessedInformationRecorder;
+import org.apache.myfaces.extensions.validator.core.factory.FactoryFinder;
+import org.apache.myfaces.extensions.validator.core.factory.DefaultFactoryFinder;
+import org.apache.myfaces.extensions.validator.core.initializer.configuration.StaticConfigurationNames;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.util.ClassUtils;
+import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@UsageInformation(UsageCategory.API)
+public class ExtValContext
+{
+    private final Log logger = LogFactory.getLog(getClass());
+
+    private static ExtValContext extValContext = new ExtValContext();
+
+    private FactoryFinder factoryFinder = new DefaultFactoryFinder();
+    private Map<String, RendererInterceptor> rendererInterceptors = new HashMap<String, RendererInterceptor>();
+    private List<String> deniedInterceptors = new ArrayList<String>();
+    private List<ProcessedInformationRecorder> processedInformationRecorders =
+        new ArrayList<ProcessedInformationRecorder>();
+
+    private List<ComponentInitializer> componentInitializers;
+    private List<ValidationExceptionInterceptor> validationExceptionInterceptors;
+
+    private Map<String, Object> globalProperties = new HashMap<String, Object>();
+
+    private Map<StaticConfigurationNames, List<StaticConfiguration<String, String>>> staticConfigMap
+        = new HashMap<StaticConfigurationNames, List<StaticConfiguration<String, String>>>();
+
+
+    private void lazyInitComponentInitializers()
+    {
+        if(this.componentInitializers != null)
+        {
+            return;
+        }
+
+        this.componentInitializers = new ArrayList<ComponentInitializer>();
+        List<String> componentInitializerClassNames = new ArrayList<String>();
+        componentInitializerClassNames
+            .add(WebXmlParameter.CUSTOM_COMPONENT_INITIALIZER);
+        componentInitializerClassNames
+            .add(ExtValContext.getContext().getInformationProviderBean().get(CustomInformation.COMPONENT_INITIALIZER));
+
+        ComponentInitializer componentInitializer;
+        for (String componentInitializerName : componentInitializerClassNames)
+        {
+            componentInitializer =
+                (ComponentInitializer) ClassUtils.tryToInstantiateClassForName(componentInitializerName);
+
+            if (componentInitializer != null)
+            {
+                componentInitializers.add(componentInitializer);
+
+                if(logger.isTraceEnabled())
+                {
+                    logger.trace(componentInitializer.getClass().getName() + " added");
+                }
+            }
+        }
+    }
+
+    private void lazyInitValidationExceptionInterceptors()
+    {
+        if(this.validationExceptionInterceptors != null)
+        {
+            return;
+        }
+
+        this.validationExceptionInterceptors = new ArrayList<ValidationExceptionInterceptor>();
+        List<String> validationExceptionInterceptorClassNames = new ArrayList<String>();
+
+        validationExceptionInterceptorClassNames
+            .add(WebXmlParameter.CUSTOM_VALIDATION_EXCEPTION_INTERCEPTOR);
+        validationExceptionInterceptorClassNames
+            .add(ExtValContext.getContext().getInformationProviderBean().get(
+                    CustomInformation.VALIDATION_EXCEPTION_INTERCEPTOR));
+
+        ValidationExceptionInterceptor validationExceptionInterceptor;
+        for (String validationExceptionInterceptorName : validationExceptionInterceptorClassNames)
+        {
+            validationExceptionInterceptor =
+                (ValidationExceptionInterceptor)
+                        ClassUtils.tryToInstantiateClassForName(validationExceptionInterceptorName);
+
+            if (validationExceptionInterceptor != null)
+            {
+                validationExceptionInterceptors.add(validationExceptionInterceptor);
+
+                if(logger.isTraceEnabled())
+                {
+                    logger.trace(validationExceptionInterceptor.getClass().getName() + " added");
+                }
+            }
+        }
+    }
+
+    public static ExtValContext getContext()
+    {
+        return extValContext;
+    }
+
+    public FactoryFinder getFactoryFinder()
+    {
+        return factoryFinder;
+    }
+
+    public void setFactoryFinder(FactoryFinder factoryFinder)
+    {
+        if(factoryFinder != null)
+        {
+            this.factoryFinder = factoryFinder;
+        }
+    }
+
+    public List<RendererInterceptor> getRendererInterceptors()
+    {
+        return new ArrayList<RendererInterceptor>(rendererInterceptors.values());
+    }
+
+    public boolean registerRendererInterceptor(RendererInterceptor rendererInterceptor)
+    {
+        synchronized (ExtValContext.class)
+        {
+            if (deniedInterceptors.contains(rendererInterceptor.getInterceptorId()))
+            {
+                return false;
+            }
+
+            rendererInterceptors.put(rendererInterceptor.getInterceptorId(), rendererInterceptor);
+        }
+        return true;
+    }
+
+    public void deregisterRendererInterceptor(Class rendererInterceptorClass)
+    {
+        RendererInterceptor rendererInterceptor =
+            (RendererInterceptor) ClassUtils.tryToInstantiateClass(rendererInterceptorClass);
+
+        synchronized (ExtValContext.class)
+        {
+            rendererInterceptors.remove(rendererInterceptor.getInterceptorId());
+        }
+    }
+
+    //if an interceptor hasn't been registered so far, it should be denied at future registrations
+    public void denyRendererInterceptor(Class rendererInterceptorClass)
+    {
+        RendererInterceptor rendererInterceptor =
+            (RendererInterceptor) ClassUtils.tryToInstantiateClass(rendererInterceptorClass);
+
+        synchronized (ExtValContext.class)
+        {
+            deniedInterceptors.add(rendererInterceptor.getInterceptorId());
+        }
+        deregisterRendererInterceptor(rendererInterceptorClass);
+    }
+
+    public void addComponentInitializer(ComponentInitializer componentInitializer)
+    {
+        lazyInitComponentInitializers();
+        this.componentInitializers.add(componentInitializer);
+    }
+
+    public List<ComponentInitializer> getComponentInitializers()
+    {
+        lazyInitComponentInitializers();
+        return componentInitializers;
+    }
+
+    public void addValidationExceptionInterceptor(ValidationExceptionInterceptor validationExceptionInterceptor)
+    {
+        lazyInitValidationExceptionInterceptors();
+        this.validationExceptionInterceptors.add(validationExceptionInterceptor);
+    }
+
+    public List<ValidationExceptionInterceptor> getValidationExceptionInterceptors()
+    {
+        lazyInitValidationExceptionInterceptors();
+        return validationExceptionInterceptors;
+    }
+
+    public List<ProcessedInformationRecorder> getProcessedInformationRecorders()
+    {
+        return processedInformationRecorders;
+    }
+
+    public void addProcessedInformationRecorder(ProcessedInformationRecorder processedInformationRecorder)
+    {
+        this.processedInformationRecorders.add(processedInformationRecorder);
+    }
+
+    public InformationProviderBean getInformationProviderBean()
+    {
+        Map applicationMap = FacesContext.getCurrentInstance().getExternalContext().getApplicationMap();
+        InformationProviderBean bean = (InformationProviderBean) applicationMap.get(InformationProviderBean.BEAN_NAME);
+
+        if (bean == null)
+        {
+            return initInformationProviderBean(applicationMap);
+        }
+        return bean;
+    }
+
+    private InformationProviderBean initInformationProviderBean(Map applicationMap)
+    {
+        List<String> informationProviderBeanClassNames = new ArrayList<String>();
+
+        informationProviderBeanClassNames.add(WebXmlParameter.CUSTOM_INFORMATION_PROVIDER_BEAN);
+        informationProviderBeanClassNames.add(InformationProviderBean.CUSTOM_BEAN);
+
+        InformationProviderBean informationProviderBean;
+        for (String className : informationProviderBeanClassNames)
+        {
+            informationProviderBean = (InformationProviderBean) ClassUtils.tryToInstantiateClassForName(className);
+
+            if (informationProviderBean != null)
+            {
+                applicationMap.put(InformationProviderBean.BEAN_NAME, informationProviderBean);
+                return informationProviderBean;
+            }
+        }
+
+        tryToInitCustomConfiguredInformationProviderBeanClassName(applicationMap);
+
+        if(applicationMap.containsKey(InformationProviderBean.BEAN_NAME))
+        {
+            return (InformationProviderBean)applicationMap.get(InformationProviderBean.BEAN_NAME);
+        }
+        return new InformationProviderBean();
+    }
+
+    private void tryToInitCustomConfiguredInformationProviderBeanClassName(Map applicationMap)
+    {
+        InformationProviderBean bean = (InformationProviderBean) ExtValUtils.getELHelper()
+            .getBean(InformationProviderBean.CUSTOM_BEAN.replace(".", "_"));
+
+        if(bean != null)
+        {
+            applicationMap.put(InformationProviderBean.BEAN_NAME, bean);
+        }
+    }
+
+    public List<StaticConfiguration<String, String>> getStaticConfiguration(StaticConfigurationNames name)
+    {
+        if(!this.staticConfigMap.containsKey(name))
+        {
+            List<StaticConfiguration<String, String>> staticConfigList =
+                    new ArrayList<StaticConfiguration<String, String>>();
+            this.staticConfigMap.put(name, staticConfigList);
+        }
+        return this.staticConfigMap.get(name);
+    }
+
+    public void addStaticConfiguration(StaticConfigurationNames name, StaticConfiguration<String, String> staticConfig)
+    {
+        synchronized (this)
+        {
+            List<StaticConfiguration<String, String>> staticConfigList;
+            if(!this.staticConfigMap.containsKey(name))
+            {
+                staticConfigList = new ArrayList<StaticConfiguration<String, String>>();
+                this.staticConfigMap.put(name, staticConfigList);
+            }
+            this.staticConfigMap.get(name).add(staticConfig);
+        }
+    }
+
+    public boolean addGlobalProperty(String name, Object value)
+    {
+        return addGlobalProperty(name , value, true);
+    }
+
+    public boolean addGlobalProperty(String name, Object value, boolean forceOverride)
+    {
+        if(this.globalProperties.containsKey(name))
+        {
+            if(!forceOverride)
+            {
+                return false;
+            }
+            else
+            {
+                if(this.logger.isInfoEnabled())
+                {
+                    logger.info("override global property '" + name + "'");
+                }
+            }
+        }
+
+        this.globalProperties.put(name, value);
+        return true;
+    }
+
+    public Object getGlobalProperty(String name)
+    {
+        return this.globalProperties.get(name);
+    }
+}
---
+++ prepare1_0_1rc_test/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/crossval/strategy/DateIsStrategy.java
@@ -0,0 +1,201 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.crossval.strategy;
+
+import org.apache.myfaces.extensions.validator.crossval.CrossValidationStorageEntry;
+import org.apache.myfaces.extensions.validator.crossval.annotation.DateIs;
+import org.apache.myfaces.extensions.validator.crossval.annotation.DateIsType;
+import org.apache.myfaces.extensions.validator.baseval.annotation.SkipValidationSupport;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+
+import javax.faces.context.FacesContext;
+import java.lang.annotation.Annotation;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.MissingResourceException;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@SkipValidationSupport
+@UsageInformation(UsageCategory.INTERNAL)
+public class DateIsStrategy extends AbstractCompareStrategy
+{
+    protected static final String TOO_EARLY = "early";
+    protected static final String TOO_LATE = "late";
+    protected static final String NOT_EQUAL_DATE_TIME = "not equal";
+    protected static final String RESULT_KEY = "result";
+    protected static final String COMPARED_VALUE_KEY = "target value";
+
+    public boolean useTargetComponentToDisplayErrorMsg(
+            CrossValidationStorageEntry crossValidationStorageEntry)
+    {
+        return true;
+    }
+
+    public boolean isViolation(Object object1, Object object2, Annotation annotation)
+    {
+        boolean violationFound;
+
+        if (((DateIs) annotation).type().equals(DateIsType.same))
+        {
+            violationFound = object1 != null && !object1.equals(object2);
+
+            if (violationFound)
+            {
+                this.violationResultStorage.put(RESULT_KEY, NOT_EQUAL_DATE_TIME);
+            }
+        }
+        else if (((DateIs) annotation).type().equals(DateIsType.before))
+        {
+            violationFound = object1 != null && object2 != null &&
+                            (!new Date(((Date) object1).getTime()).before((Date) object2) || object1.equals(object2));
+
+            if (violationFound)
+            {
+                this.violationResultStorage.put(RESULT_KEY, TOO_LATE);
+            }
+        }
+        else
+        {
+            violationFound = object1 != null && object2 != null &&
+                            (!new Date(((Date) object1).getTime()).after((Date) object2) || object1.equals(object2));
+
+            if (violationFound)
+            {
+                this.violationResultStorage.put(RESULT_KEY, TOO_EARLY);
+            }
+        }
+
+        if (violationFound)
+        {
+            this.violationResultStorage.put(COMPARED_VALUE_KEY, object1);
+        }
+
+        return violationFound;
+    }
+
+    public String[] getValidationTargets(Annotation annotation)
+    {
+        return ((DateIs) annotation).valueOf();
+    }
+
+    /*
+     * protected
+     */
+    protected String getValidationErrorMsgKey(Annotation annotation, boolean isTargetComponent)
+    {
+        if (!isTargetComponent)
+        {
+            return null;
+        }
+
+        String result = (String) this.violationResultStorage.get(RESULT_KEY);
+        if (TOO_EARLY.equals(result))
+        {
+            return getNotAfterErrorMsgKey((DateIs) annotation);
+        }
+        else if (TOO_LATE.equals(result))
+        {
+            return getNotBeforeErrorMsgKey((DateIs) annotation);
+        }
+        else
+        {
+            return getNotEqualErrorMsgKey((DateIs) annotation);
+        }
+    }
+
+    @Override
+    protected String getErrorMessageSummary(Annotation annotation, boolean isTargetComponent)
+    {
+        if (!isTargetComponent)
+        {
+            return super.getErrorMessageSummary(annotation, isTargetComponent);
+        }
+
+        return getErrorMessage(getValidationErrorMsgKey(annotation, isTargetComponent), annotation, isTargetComponent);
+    }
+
+    @Override
+    protected String getErrorMessageDetail(Annotation annotation, boolean isTargetComponent)
+    {
+        if (!isTargetComponent)
+        {
+            return super.getErrorMessageDetail(annotation, isTargetComponent);
+        }
+
+        try
+        {
+            return getErrorMessage(getValidationErrorMsgKey(annotation, isTargetComponent)
+                    + DETAIL_MESSAGE_KEY_POSTFIX, annotation, isTargetComponent);
+        }
+        catch (MissingResourceException e)
+        {
+            if(logger.isWarnEnabled())
+            {
+                logger.warn("couldn't find key " + getValidationErrorMsgKey(annotation, isTargetComponent)
+                    + DETAIL_MESSAGE_KEY_POSTFIX, e);
+            }
+        }
+        return null;
+    }
+
+    protected String getErrorMessage(String key, Annotation annotation, boolean isTargetComponent)
+    {
+        String message = resolveMessage(key);
+
+        DateFormat dateFormat = DateFormat.getDateInstance(((DateIs) annotation).errorMessageDateStyle(),
+            FacesContext.getCurrentInstance().getViewRoot().getLocale());
+
+        //replace placeholder with the value of the other component
+        return message.replace("{0}", dateFormat.format((Date) this.violationResultStorage.get(COMPARED_VALUE_KEY)));
+    }
+
+    /*
+     * private
+     */
+    private String getNotAfterErrorMsgKey(DateIs annotation)
+    {
+        if (annotation.validationErrorMsgKey().equals(""))
+        {
+            return annotation.notAfterErrorMsgKey();
+        }
+        return annotation.validationErrorMsgKey();
+    }
+
+    private String getNotBeforeErrorMsgKey(DateIs annotation)
+    {
+        if (annotation.validationErrorMsgKey().equals(""))
+        {
+            return annotation.notBeforeErrorMsgKey();
+        }
+        return annotation.validationErrorMsgKey();
+    }
+
+    private String getNotEqualErrorMsgKey(DateIs annotation)
+    {
+        if (annotation.validationErrorMsgKey().equals(""))
+        {
+            return annotation.notEqualErrorMsgKey();
+        }
+        return annotation.validationErrorMsgKey();
+    }
+}
