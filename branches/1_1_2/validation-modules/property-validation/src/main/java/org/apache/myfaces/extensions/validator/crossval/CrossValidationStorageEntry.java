70263100c82d4febbe343286a9a55a44f681a646
Leonardo Uribe <lu4242@apache.org>
2009-04-15 21:09:42 +0000

    [maven-release-plugin]  copy for tag 1_1_2
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@765361 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/1_1_2/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/crossval/CrossValidationStorageEntry.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.crossval;
+
+import org.apache.myfaces.extensions.validator.core.metadata.MetaDataEntry;
+import org.apache.myfaces.extensions.validator.crossval.strategy.CrossValidationStrategy;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+
+import javax.faces.component.UIComponent;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@UsageInformation(UsageCategory.API)
+public class CrossValidationStorageEntry
+{
+    private MetaDataEntry metaDataEntry;
+    //for complex components (e.g. a table) stores the object of entry (#{entry.property})
+    private UIComponent component;
+    private Object convertedObject;
+    private CrossValidationStrategy validationStrategy;
+    //just for input components within complex components e.g. dataTable,...
+    private String clientId;
+
+    public MetaDataEntry getMetaDataEntry()
+    {
+        return metaDataEntry;
+    }
+
+    public void setMetaDataEntry(MetaDataEntry metaDataEntry)
+    {
+        this.metaDataEntry = metaDataEntry;
+    }
+
+    public UIComponent getComponent()
+    {
+        return component;
+    }
+
+    public void setComponent(UIComponent component)
+    {
+        this.component = component;
+    }
+
+    public Object getConvertedObject()
+    {
+        return convertedObject;
+    }
+
+    public void setConvertedObject(Object convertedObject)
+    {
+        this.convertedObject = convertedObject;
+    }
+
+    public CrossValidationStrategy getValidationStrategy()
+    {
+        return validationStrategy;
+    }
+
+    public void setValidationStrategy(CrossValidationStrategy validationStrategy)
+    {
+        this.validationStrategy = validationStrategy;
+    }
+
+    public String getClientId()
+    {
+        return clientId;
+    }
+
+    public void setClientId(String clientId)
+    {
+        this.clientId = clientId;
+    }
+}
