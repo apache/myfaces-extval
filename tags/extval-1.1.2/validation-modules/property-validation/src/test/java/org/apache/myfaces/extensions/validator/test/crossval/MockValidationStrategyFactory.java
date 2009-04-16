407c09ed4e9afe3b735db36c37c01d1f1323003c
Gerhard Petracek <gpetracek@apache.org>
2009-04-16 01:05:39 +0000

    release tag of extval v1.1.2
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@765443 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tags/extval-1.1.2/validation-modules/property-validation/src/test/java/org/apache/myfaces/extensions/validator/test/crossval/MockValidationStrategyFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.test.crossval;
+
+import org.apache.myfaces.extensions.validator.core.validation.strategy.DefaultValidationStrategyFactory;
+import org.apache.myfaces.extensions.validator.core.validation.strategy.ValidationStrategy;
+import org.apache.myfaces.extensions.validator.core.metadata.MetaDataEntry;
+import org.apache.myfaces.extensions.validator.util.ReflectionUtils;
+
+/**
+ * @author Gerhard Petracek
+ */
+public class MockValidationStrategyFactory extends DefaultValidationStrategyFactory
+{
+    @Override
+    public ValidationStrategy create(String metaDataKey)
+    {
+        //force init so that every test-case setup method can add a mock validation strategy via extval java-api
+        ReflectionUtils.tryToInvokeMethod(this,ReflectionUtils.tryToGetMethod(getClass(), "initStaticMappings"));
+        return super.create(metaDataKey);
+    }
+}
