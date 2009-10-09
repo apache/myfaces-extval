6896b09eff57081cddd221686c0419938d138978
Gerhard Petracek <gpetracek@apache.org>
2009-10-09 12:56:34 +0000

    minor refactorings
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@823535 13f79535-47bb-0310-9956-ffa450edef68

--- branch_for_jsf_1_1/validation-modules/bean-validation/src/main/java/org/apache/myfaces/extensions/validator/beanval/annotation/BeanValidation.java
+++ branch_for_jsf_1_1/validation-modules/bean-validation/src/main/java/org/apache/myfaces/extensions/validator/beanval/annotation/BeanValidation.java
@@ -26,6 +26,7 @@ import org.apache.myfaces.extensions.validator.internal.Priority;
 import javax.validation.groups.Default;
 import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
+import java.lang.annotation.Documented;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.ElementType.METHOD;
@@ -39,6 +40,7 @@ import static java.lang.annotation.ElementType.FIELD;
 @Target({METHOD, FIELD, TYPE})
 @Retention(RUNTIME)
 @UsageInformation(UsageCategory.API)
+@Documented
 public @interface BeanValidation
 {
     String[] viewIds() default "*";
