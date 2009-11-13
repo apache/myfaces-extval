01308fed297f98c7f45dc2cdfff6be84aab92c0c
Gerhard Petracek <gpetracek@apache.org>
2009-11-13 02:48:45 +0000

    extval 2.0.3 m2 based on extval 1.2.3 m2 (sync)
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/branches@835712 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branch_for_jsf_2_0/core/src/main/java/org/apache/myfaces/extensions/validator/core/interceptor/ValidationInterceptor.java
@@ -0,0 +1,205 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.core.interceptor;
+
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.core.validation.strategy.ValidationStrategy;
+import org.apache.myfaces.extensions.validator.core.validation.SkipValidationEvaluator;
+import org.apache.myfaces.extensions.validator.core.metadata.extractor.MetaDataExtractor;
+import org.apache.myfaces.extensions.validator.core.metadata.MetaDataEntry;
+import org.apache.myfaces.extensions.validator.core.property.PropertyInformation;
+import org.apache.myfaces.extensions.validator.core.ExtValContext;
+import org.apache.myfaces.extensions.validator.core.renderkit.exception.SkipAfterInterceptorsException;
+import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+
+import javax.faces.context.FacesContext;
+import javax.faces.component.UIComponent;
+import javax.faces.component.EditableValueHolder;
+import javax.faces.render.Renderer;
+import java.util.Map;
+import java.lang.annotation.Annotation;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@UsageInformation(UsageCategory.INTERNAL)
+public class ValidationInterceptor extends AbstractValidationInterceptor
+{
+    @Override
+    public void afterDecode(FacesContext facesContext, UIComponent uiComponent, Renderer wrapped)
+            throws SkipAfterInterceptorsException
+    {
+        /*
+         * component initialization sets a component to required if there are constraints which indicate it
+         * the required flag in a component leads to problems with h:messages (additional message) as well as
+         * incompatibilities with skip validation and severities
+         */
+        if(uiComponent instanceof EditableValueHolder)
+        {
+            ((EditableValueHolder)uiComponent).setRequired(false);
+        }
+    }
+
+    protected void initComponent(FacesContext facesContext, UIComponent uiComponent)
+    {
+        if(logger.isTraceEnabled())
+        {
+            logger.trace("start to init component " + uiComponent.getClass().getName());
+        }
+
+        Map<String, Object> metaDataResult = ExtValUtils
+                .getTransformedMetaDataFor(facesContext, uiComponent, getModuleKey());
+
+        //get component initializer for the current component and configure it
+        //also in case of skipped validation to reset e.g. the required attribute
+        if(!metaDataResult.isEmpty())
+        {
+            ExtValUtils.configureComponentWithMetaData(facesContext, uiComponent, metaDataResult);
+        }
+
+        if(logger.isTraceEnabled())
+        {
+            logger.trace("init component of " + uiComponent.getClass().getName() + " finished");
+        }
+    }
+
+    protected void processValidation(FacesContext facesContext, UIComponent uiComponent, Object convertedObject)
+    {
+        MetaDataExtractor metaDataExtractor = ExtValUtils.getComponentMetaDataExtractorFor(getModuleKey());
+
+        PropertyInformation propertyInformation = metaDataExtractor.extract(facesContext, uiComponent);
+
+        if(!ExtValUtils.executeGlobalBeforeValidationInterceptors(facesContext, uiComponent, convertedObject,
+                PropertyInformation.class.getName() ,propertyInformation, getModuleKey()))
+        {
+            return;
+        }
+
+        try
+        {
+            if(logger.isTraceEnabled())
+            {
+                logger.trace("start validation");
+            }
+
+            processFieldValidation(facesContext, uiComponent, convertedObject, propertyInformation);
+        }
+        finally
+        {
+            if(logger.isTraceEnabled())
+            {
+                logger.trace("validation finished");
+            }
+
+            ExtValUtils.executeGlobalAfterValidationInterceptors(facesContext, uiComponent, convertedObject,
+                    PropertyInformation.class.getName(), propertyInformation, getModuleKey());
+        }
+    }
+
+    protected void processFieldValidation(FacesContext facesContext,
+                                          UIComponent uiComponent,
+                                          Object convertedObject,
+                                          PropertyInformation propertyInformation)
+    {
+        ValidationStrategy validationStrategy;
+        SkipValidationEvaluator skipValidationEvaluator = ExtValContext.getContext().getSkipValidationEvaluator();
+        for (MetaDataEntry entry : propertyInformation.getMetaDataEntries())
+        {
+            validationStrategy = ExtValUtils.getValidationStrategyForMetaData(entry.getKey());
+
+            if (validationStrategy != null)
+            {
+                if(skipValidationEvaluator.skipValidation(facesContext, uiComponent, validationStrategy, entry))
+                {
+                    if(logger.isTraceEnabled())
+                    {
+                        logger.trace("skip validation of " + entry.getValue() +
+                                " with " + validationStrategy.getClass().getName());
+                    }
+                    //don't break maybe there are constraints which don't support the skip-mechanism
+                    continue;
+                }
+
+                if(logger.isTraceEnabled())
+                {
+                    logger.trace("validate " + entry.getValue() + " with " + validationStrategy.getClass().getName());
+                }
+
+                try
+                {
+                    if(entry.getValue() instanceof Annotation)
+                    {
+                        if(!ExtValUtils.executeLocalBeforeValidationInterceptors(
+                                facesContext, uiComponent, convertedObject,
+                                PropertyInformation.class.getName(), propertyInformation,
+                                entry.getValue(Annotation.class)))
+                        {
+                            continue;
+                        }
+                    }
+
+                    /*
+                     * validation
+                     */
+                    validationStrategy.validate(facesContext, uiComponent, entry, convertedObject);
+                }
+                finally
+                {
+                    if(entry.getValue() instanceof Annotation)
+                    {
+                        ExtValUtils.executeLocalAfterValidationInterceptors(
+                                facesContext, uiComponent, convertedObject,
+                                PropertyInformation.class.getName(), propertyInformation,
+                                entry.getValue(Annotation.class));
+                    }
+                }
+            }
+            else
+            {
+                if(logger.isTraceEnabled())
+                {
+                    logger.trace("no validation strategy found for " + entry.getValue());
+                }
+            }
+        }
+    }
+
+    @Override
+    /**
+     * to ensure backward compatibility
+     */
+    protected boolean interpretEmptyStringValuesAsNull()
+    {
+        return false;
+    }
+
+    @Override
+    protected boolean recordProcessedInformation()
+    {
+        return true;
+    }
+
+    protected Class getModuleKey()
+    {
+        //override if needed
+        return null;
+    }
+}
---
+++ branch_for_jsf_2_0/core/src/main/java/org/apache/myfaces/extensions/validator/core/renderkit/ExtValRenderKitFactory.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.core.renderkit;
+
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.core.ExtValContext;
+import org.apache.myfaces.extensions.validator.core.factory.FactoryNames;
+import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.faces.render.RenderKitFactory;
+import javax.faces.render.RenderKit;
+import javax.faces.context.FacesContext;
+import java.util.Iterator;
+
+/**
+ * central mechanism which is responsible to create a wrapper for a renderer - starting point of extval.
+ *
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@UsageInformation(UsageCategory.INTERNAL)
+public class ExtValRenderKitFactory extends RenderKitFactory
+{
+    protected final Log logger = LogFactory.getLog(getClass());
+    private RenderKitFactory wrapped;
+    private DefaultRenderKitWrapperFactory defaultRenderKitWrapperFactory = new DefaultRenderKitWrapperFactory();
+
+    public ExtValRenderKitFactory(RenderKitFactory renderKitFactory)
+    {
+        this.wrapped = renderKitFactory;
+
+        if(logger.isDebugEnabled())
+        {
+            logger.debug(getClass().getName() + " instantiated");
+        }
+    }
+
+    public void addRenderKit(String s, RenderKit renderKit)
+    {
+        wrapped.addRenderKit(s, renderKit);
+    }
+
+    public RenderKit getRenderKit(FacesContext facesContext, String s)
+    {
+        RenderKit renderKit = this.wrapped.getRenderKit(facesContext, s);
+
+        //jsf ri + trinidad
+        if(renderKit == null)
+        {
+            return null;
+        }
+
+        //test early config in case of mojarra
+        if(!ExtValUtils.isApplicationInitialized())
+        {
+            return this.defaultRenderKitWrapperFactory.createWrapper(renderKit);
+        }
+
+        return tryToCreateWrapperWithWrapperFactory(renderKit);
+    }
+
+    private RenderKit tryToCreateWrapperWithWrapperFactory(RenderKit renderKit)
+    {
+        AbstractRenderKitWrapperFactory wrapperFactory = ExtValContext.getContext().getFactoryFinder()
+            .getFactory(FactoryNames.RENDERKIT_WRAPPER_FACTORY, AbstractRenderKitWrapperFactory.class);
+
+        //some component libs e.g. myfaces-trinidad aren't compatible with this clean approach
+        //example see TrinidadModuleStartupListener
+        if(wrapperFactory.isDeactivated())
+        {
+            return renderKit;
+        }
+
+        return wrapperFactory.create(renderKit);
+    }
+
+    public Iterator<String> getRenderKitIds()
+    {
+        return wrapped.getRenderKitIds();
+    }
+}
