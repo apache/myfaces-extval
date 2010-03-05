890c69f0f060fbce83edd79239fed632b66cafa1
Gerhard Petracek <gpetracek@apache.org>
2010-03-05 18:23:01 +0000

    EXTVAL-83 initial implementation
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@919528 13f79535-47bb-0310-9956-ffa450edef68

---
+++ branches/experimental/trunk_with_extval-83/core/src/main/java/org/apache/myfaces/extensions/validator/core/storage/mapper/DefaultMappedConstraintSourceStorageNameMapper.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.core.storage.mapper;
+
+import org.apache.myfaces.extensions.validator.core.InvocationOrder;
+import org.apache.myfaces.extensions.validator.core.mapper.NameMapper;
+import org.apache.myfaces.extensions.validator.core.storage.MappedConstraintSourceStorage;
+import org.apache.myfaces.extensions.validator.core.storage.DefaultMappedConstraintSourceStorage;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+
+/**
+ * use a public class to allow optional deregistration
+ *
+ * @author Gerhard Petracek
+ * @since x.x.4
+ */
+@InvocationOrder(100)
+@UsageInformation(UsageCategory.INTERNAL)
+public class DefaultMappedConstraintSourceStorageNameMapper implements NameMapper<String>
+{
+    public String createName(String source)
+    {
+        return (MappedConstraintSourceStorage.class.getName().equals(source)) ?
+                DefaultMappedConstraintSourceStorage.class.getName() : null;
+    }
+}
---
+++ branches/experimental/trunk_with_extval-83/validation-modules/bean-validation/src/main/java/org/apache/myfaces/extensions/validator/beanval/MappedConstraintSourceBeanValidationModuleValidationInterceptor.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.beanval;
+
+import org.apache.myfaces.extensions.validator.core.property.PropertyDetails;
+import org.apache.myfaces.extensions.validator.core.property.PropertyInformation;
+import org.apache.myfaces.extensions.validator.core.property.PropertyInformationKeys;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+/**
+ * @author Gerhard Petracek
+ * @since x.x.4
+ */
+@UsageInformation(UsageCategory.INTERNAL)
+public class MappedConstraintSourceBeanValidationModuleValidationInterceptor
+        extends BeanValidationModuleValidationInterceptor
+{
+    MappedConstraintSourceBeanValidationModuleValidationInterceptorInternals csaBviUtils =
+            new MappedConstraintSourceBeanValidationModuleValidationInterceptorInternals(this.logger, this.bviUtils);
+
+    @Override
+    protected void initComponentWithPropertyDetails(FacesContext facesContext,
+                                                    UIComponent uiComponent,
+                                                    PropertyDetails propertyDetails)
+    {
+        this.csaBviUtils.initComponentWithPropertyDetailsOfMappedConstraintSource(
+                facesContext, uiComponent, propertyDetails);
+    }
+
+    @Override
+    protected boolean hasBeanValidationConstraints(PropertyInformation propertyInformation)
+    {
+        PropertyDetails propertyDetails = propertyInformation.getInformation(
+                PropertyInformationKeys.PROPERTY_DETAILS, PropertyDetails.class);
+
+        return this.csaBviUtils.resolveMappedConstraintSourceFor(propertyDetails.getKey(),
+                                                              propertyDetails.getBaseObject().getClass(),
+                                                              propertyDetails.getProperty()) != null;
+    }
+
+    @Override
+    protected void processFieldValidation(FacesContext facesContext,
+                                          UIComponent uiComponent,
+                                          Object convertedObject,
+                                          PropertyInformation propertyInformation)
+    {
+        Set<ConstraintViolation> violations = this.csaBviUtils
+                .validateMappedConstraintSource(facesContext, uiComponent, convertedObject, propertyInformation);
+
+        processConstraintViolations(facesContext, uiComponent, convertedObject, violations);
+    }
+}
