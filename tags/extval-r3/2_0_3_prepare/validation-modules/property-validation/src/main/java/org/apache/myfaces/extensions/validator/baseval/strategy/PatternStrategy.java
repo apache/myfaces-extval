030504273c9945e9f74991c1f3547addfbb580b5
Gerhard Petracek <gpetracek@apache.org>
2010-03-14 22:16:36 +0000

    tag of release preparation base
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator@922982 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tags/extval-r3/2_0_3_prepare/validation-modules/property-validation/src/main/java/org/apache/myfaces/extensions/validator/baseval/strategy/PatternStrategy.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.baseval.strategy;
+
+import org.apache.myfaces.extensions.validator.baseval.annotation.Pattern;
+import org.apache.myfaces.extensions.validator.baseval.annotation.SkipValidationSupport;
+import org.apache.myfaces.extensions.validator.core.metadata.MetaDataEntry;
+import org.apache.myfaces.extensions.validator.core.validation.strategy.AbstractAnnotationValidationStrategy;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * @author Gerhard Petracek
+ * @since 1.x.1
+ */
+@SkipValidationSupport
+@UsageInformation(UsageCategory.INTERNAL)
+public class PatternStrategy extends AbstractAnnotationValidationStrategy<Pattern>
+{
+    protected void processValidation(FacesContext facesContext,
+            UIComponent uiComponent, MetaDataEntry metaDataEntry,
+            Object convertedObject) throws ValidatorException
+    {
+        Pattern annotation = metaDataEntry.getValue(Pattern.class);
+
+        for (String expression : annotation.value())
+        {
+            if (!java.util.regex.Pattern.compile(expression).matcher(convertedObject.toString()).matches())
+            {
+                throw new ValidatorException(new FacesMessage(
+                        FacesMessage.SEVERITY_ERROR,
+                        getErrorMessageSummary(annotation),
+                        getErrorMessageDetail(annotation).replace("{0}", expression)));
+            }
+        }
+    }
+
+    protected String getValidationErrorMsgKey(Pattern annotation)
+    {
+        return annotation.validationErrorMsgKey();
+    }
+}
