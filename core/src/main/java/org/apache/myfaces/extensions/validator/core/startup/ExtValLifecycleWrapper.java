bb1638f5a9540702c660c2966e216814778b74ca
Rudy De Busscher <rdebusscher@apache.org>
2011-01-06 08:01:09 +0000

    EXTVAL-123
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/trunk@1055780 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/src/main/java/org/apache/myfaces/extensions/validator/core/startup/ExtValLifecycleWrapper.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.myfaces.extensions.validator.core.startup;
+
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
+
+import javax.faces.FacesException;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseListener;
+import javax.faces.lifecycle.Lifecycle;
+
+/**
+ * Wrapper around a Lifecycle that initialise the ExtVal PhaseListeners before an execution of any
+ * phase (except the render view). Solution for the issue EXTVAL-123.
+ * @author Rudy De Busscher
+ * @author Gerard Petracek
+ * @since x.x.5
+ */
+@UsageInformation(value = UsageCategory.INTERNAL)
+class ExtValLifecycleWrapper extends Lifecycle
+{
+    private Lifecycle wrapped;
+
+    ExtValLifecycleWrapper(Lifecycle wrapped)
+    {
+        this.wrapped = wrapped;
+
+    }
+
+    public void addPhaseListener(PhaseListener phaseListener)
+    {
+        wrapped.addPhaseListener(phaseListener);
+    }
+
+    public void execute(FacesContext facesContext)
+            throws FacesException
+    {
+        initializeExtVal();
+        wrapped.execute(facesContext);
+    }
+
+    public PhaseListener[] getPhaseListeners()
+    {
+        return wrapped.getPhaseListeners();
+    }
+
+    public void removePhaseListener(PhaseListener phaseListener)
+    {
+        wrapped.removePhaseListener(phaseListener);
+    }
+
+    public void render(FacesContext facesContext)
+            throws FacesException
+    {
+        // Don't need to initialize ExtVal here because this phase is never the very first one in a system (restore
+        // view is always the first)
+        wrapped.render(facesContext);
+    }
+
+
+    private void initializeExtVal()
+    {
+
+        for (PhaseListener currentPhaseListener : getPhaseListeners())
+        {
+            if (currentPhaseListener instanceof AbstractStartupListener)
+            {
+                currentPhaseListener.beforePhase(null);
+            }
+        }
+    }
+
+}
