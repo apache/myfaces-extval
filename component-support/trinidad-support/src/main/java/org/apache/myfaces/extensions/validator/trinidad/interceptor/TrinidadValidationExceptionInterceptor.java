40b26587da3dce5842743315f911861996b19c71
Gerhard Petracek <gpetracek@apache.org>
2011-07-13 19:26:25 +0000

    EXTVAL-134 support for CoreSelectOneChoice - thx to Andrei Lupsa
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/validator/trunk@1146264 13f79535-47bb-0310-9956-ffa450edef68

--- component-support/trinidad-support/src/main/java/org/apache/myfaces/extensions/validator/trinidad/interceptor/TrinidadValidationExceptionInterceptor.java
+++ component-support/trinidad-support/src/main/java/org/apache/myfaces/extensions/validator/trinidad/interceptor/TrinidadValidationExceptionInterceptor.java
@@ -18,23 +18,23 @@
  */
 package org.apache.myfaces.extensions.validator.trinidad.interceptor;
 
-import org.apache.myfaces.extensions.validator.internal.UsageInformation;
-import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.core.InvocationOrder;
 import org.apache.myfaces.extensions.validator.core.interceptor.ValidationExceptionInterceptor;
 import org.apache.myfaces.extensions.validator.core.metadata.MetaDataEntry;
 import org.apache.myfaces.extensions.validator.core.property.PropertyInformationKeys;
-import org.apache.myfaces.extensions.validator.core.validation.strategy.ValidationStrategy;
-import org.apache.myfaces.extensions.validator.core.validation.message.LabeledMessage;
 import org.apache.myfaces.extensions.validator.core.validation.exception.RequiredValidatorException;
-import org.apache.myfaces.extensions.validator.core.InvocationOrder;
-import org.apache.myfaces.extensions.validator.util.ReflectionUtils;
+import org.apache.myfaces.extensions.validator.core.validation.message.LabeledMessage;
+import org.apache.myfaces.extensions.validator.core.validation.strategy.ValidationStrategy;
+import org.apache.myfaces.extensions.validator.internal.UsageCategory;
+import org.apache.myfaces.extensions.validator.internal.UsageInformation;
 import org.apache.myfaces.extensions.validator.util.ExtValUtils;
+import org.apache.myfaces.extensions.validator.util.ReflectionUtils;
 import org.apache.myfaces.trinidad.context.RequestContext;
 
-import javax.faces.context.FacesContext;
-import javax.faces.component.UIComponent;
-import javax.faces.validator.ValidatorException;
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 /**
  * @author Gerhard Petracek
@@ -45,9 +45,11 @@ import javax.faces.application.FacesMessage;
 public class TrinidadValidationExceptionInterceptor implements ValidationExceptionInterceptor
 {
     private static final String TRINIDAD_CORE_INPUT_TEXT
-                                         = "org.apache.myfaces.trinidad.component.core.input.CoreInputText";
+            = "org.apache.myfaces.trinidad.component.core.input.CoreInputText";
     private static final String TRINIDAD_CORE_INPUT_DATE
-                                         = "org.apache.myfaces.trinidad.component.core.input.CoreInputDate";
+            = "org.apache.myfaces.trinidad.component.core.input.CoreInputDate";
+    private static final String TRINIDAD_CORE_SELECT_ONE_CHOICE
+            = "org.apache.myfaces.trinidad.component.core.input.CoreSelectOneChoice";
 
     public boolean afterThrowing(UIComponent uiComponent,
                                  MetaDataEntry metaDataEntry,
@@ -55,7 +57,7 @@ public class TrinidadValidationExceptionInterceptor implements ValidationExcepti
                                  ValidatorException validatorException,
                                  ValidationStrategy validatorExceptionSource)
     {
-        if(processComponent(uiComponent))
+        if (processComponent(uiComponent))
         {
             tryToRefreshComponent(uiComponent);
             tryToPlaceLabelInFacesMessage(uiComponent, metaDataEntry, validatorException);
@@ -85,14 +87,14 @@ public class TrinidadValidationExceptionInterceptor implements ValidationExcepti
 
     private void processLabel(FacesMessage facesMessage, String label)
     {
-        if(facesMessage instanceof LabeledMessage)
+        if (facesMessage instanceof LabeledMessage)
         {
-            ((LabeledMessage)facesMessage).setLabelText(label);
+            ((LabeledMessage) facesMessage).setLabelText(label);
         }
         //if someone uses a normal faces message
         else
         {
-            for(int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 ExtValUtils.tryToPlaceLabel(facesMessage, label, i);
             }
@@ -101,7 +103,7 @@ public class TrinidadValidationExceptionInterceptor implements ValidationExcepti
 
     private String tryToOverrideLabelIfProvidedManually(MetaDataEntry metaDataEntry, String label)
     {
-        if(metaDataEntry != null && metaDataEntry.getProperty(PropertyInformationKeys.LABEL) != null)
+        if (metaDataEntry != null && metaDataEntry.getProperty(PropertyInformationKeys.LABEL) != null)
         {
             return metaDataEntry.getProperty(PropertyInformationKeys.LABEL, String.class);
         }
@@ -110,7 +112,7 @@ public class TrinidadValidationExceptionInterceptor implements ValidationExcepti
 
     private String getClientIdAsFallbackIfNeeded(UIComponent uiComponent, String label)
     {
-        if(label == null)
+        if (label == null)
         {
             return uiComponent.getClientId(FacesContext.getCurrentInstance());
         }
@@ -119,12 +121,12 @@ public class TrinidadValidationExceptionInterceptor implements ValidationExcepti
 
     private void tryToHandleRequiredValidatorException(UIComponent uiComponent, ValidatorException validatorException)
     {
-        if(validatorException instanceof RequiredValidatorException)
+        if (validatorException instanceof RequiredValidatorException)
         {
             FacesMessage facesMessage = validatorException.getFacesMessage();
             String inlineMessage = getInlineRequiredMessage(uiComponent);
 
-            if(inlineMessage != null)
+            if (inlineMessage != null)
             {
                 facesMessage.setSummary(inlineMessage);
                 facesMessage.setDetail(inlineMessage);
@@ -134,7 +136,7 @@ public class TrinidadValidationExceptionInterceptor implements ValidationExcepti
 
     private void tryToRefreshComponent(UIComponent uiComponent)
     {
-        if(RequestContext.getCurrentInstance().isPartialRequest(FacesContext.getCurrentInstance()))
+        if (RequestContext.getCurrentInstance().isPartialRequest(FacesContext.getCurrentInstance()))
         {
             RequestContext.getCurrentInstance().addPartialTarget(uiComponent);
         }
@@ -144,18 +146,19 @@ public class TrinidadValidationExceptionInterceptor implements ValidationExcepti
     {
         return uiComponent != null &&
                 (TRINIDAD_CORE_INPUT_TEXT.equals(uiComponent.getClass().getName()) ||
-                 TRINIDAD_CORE_INPUT_DATE.equals(uiComponent.getClass().getName()));
+                        TRINIDAD_CORE_INPUT_DATE.equals(uiComponent.getClass().getName()) ||
+                        TRINIDAD_CORE_SELECT_ONE_CHOICE.equals(uiComponent.getClass().getName()));
     }
 
     private String getLabel(UIComponent uiComponent)
     {
-        return (String)ReflectionUtils.tryToInvokeMethod(uiComponent,
+        return (String) ReflectionUtils.tryToInvokeMethod(uiComponent,
                 ReflectionUtils.tryToGetMethod(uiComponent.getClass(), "getLabel"));
     }
 
     private String getInlineRequiredMessage(UIComponent uiComponent)
     {
-        return (String)ReflectionUtils.tryToInvokeMethod(uiComponent,
+        return (String) ReflectionUtils.tryToInvokeMethod(uiComponent,
                 ReflectionUtils.tryToGetMethod(uiComponent.getClass(), "getRequiredMessageDetail"));
     }
 }
